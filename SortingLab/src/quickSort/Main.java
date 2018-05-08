e4302e02ae1d40defb6f3ff7ce6d0cb9c62e9ba9
joselopeznieves <jose.lopez91@upr.edu>
2018-05-07 23:14:01 -0400

    Implement quicksort

--- SortingLab/src/quickSort/Main.java
+++ SortingLab/src/quickSort/Main.java
@@ -15,5 +15,52 @@ public class Main {
 			inputArray[i] = input.nextInt();
 		input.close();
 		
+		sort(inputArray, 0, inputArray.length -1);
+		
+		for(int i : inputArray) 
+			System.out.print(i + " ");
+		System.out.println();
+	}
+	
+
+	public static void sort(int[] arr, int low, int high) {
+		
+		if( low < high) {
+			
+			int partition = partitions(arr, low, high);
+			
+			sort(arr, low, partition -1);
+			sort(arr, partition + 1, high);
+			
+		}
+	}
+	
+	public static int partitions(int[] arr, int low, int high) {
+		int left = low, right = high - 1, pivot = arr[high]; 
+		
+		while( left <= right) {
+			while(left <= right && arr[left] <= pivot) {
+				left++;
+			}
+			while(left <= right && arr[right] >= pivot) {
+				right--;
+			}
+			
+			if(left < right) {
+				swap(arr, left, right);
+				left++;
+				right--;
+			}
+		}
+		
+		swap(arr, left, high);
+		
+		return left;
+	}
+	
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
